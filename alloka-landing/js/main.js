24c14c7298f32400101795fa559a60a4d6d4d359
Oleg Vavilov <vavilovolegik@gmail.com>
2016-09-12 18:51:05 +0300

    Fix Alloka Landing

--- alloka-landing/js/main.js
+++ alloka-landing/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 //header fixed
     $(window).on('scroll', function(){
         if ($(window).scrollTop() > 1) {
@@ -16,17 +15,16 @@ $(document).ready(function(){
     });
 
 //slider
-
     $('.preview-slider .top-layer .ground').on('click', function(){
         $(this).parent('.top-layer').fadeOut(500, function(){
             $(this).siblings('.owl-carousel').find('.owl-controls').fadeIn(0, function(){
-                $(this).parents('.preview-slider').find('.slider-tip').fadeIn(0).addClass('active');
+                $(this).parents('.preview-slider').find('.slider-tip_wrap').addClass('active').find('.slider-tip').eq(0).addClass('active');
             }).addClass('active');
         });
     });
 
-    var 
-        $carouselPrev = $('.owl-carousel');
+    var $carouselPrev = $('.owl-carousel'),
+        $nouteCarousel = $('.preview-slider');
     
     $carouselPrev.owlCarousel({
         items : 1,
@@ -39,6 +37,16 @@ $(document).ready(function(){
         navText: false,
         dots: true,
         mouseDrag: false
+    });
+
+    $nouteCarousel.on('changed.owl.carousel', function(e) {
+        var step = e.item.index - 1,
+            countN = e.item.count;
+        if(step > countN){
+            step = 1;
+        }
+        $('.slider-tip_wrap').find('.slider-tip.active').removeClass('active');
+        $('.slider-tip_wrap').find('.slider-tip').eq(step - 1).addClass('active');
     });
 
 //select about block
@@ -69,22 +77,43 @@ function textAnimate() {
     var length = $('.h3-dinamic .dinamic-text').length - 1;
     
     $('.h3-dinamic .dinamic-text').each(function(index) {
-        if($(this).hasClass('active') && index != length) {
-            $(this).removeClass('active').fadeOut(500, function(){
-                $(this).next('.dinamic-text').addClass('active').fadeIn(500);
-            });
-            return false;
-        } else if (index == length) {
-            $(this).removeClass('active').fadeOut(500, function(){
-                $('.h3-dinamic .dinamic-text').eq(0).addClass('active').fadeIn(500);
-            });
-            return false;
+        var windowWidth = $(window).width();
+        if(windowWidth > 1540){
+            if($(this).hasClass('active') && index != length) {
+                $(this).removeClass('active').fadeOut(500, function(){
+                    $(this).next('.dinamic-text').addClass('active').fadeIn(500);
+                });
+                return false;
+            } else if (index == length) {
+                $(this).removeClass('active').fadeOut(500, function(){
+                    $('.h3-dinamic .dinamic-text').eq(0).addClass('active').fadeIn(500);
+                });
+                return false;
+            }
+        } else {
+            if($(this).hasClass('active') && index != length) {
+                $(this).parent('.paragraph').fadeOut(500, function(){
+                    $(this).find('.active').removeClass('active').fadeOut(0, function(){
+                        $(this).next('.dinamic-text').addClass('active').fadeIn(0).parent('.paragraph').fadeIn(500);
+                    });
+                });
+                return false;
+            } else if (index == length) {
+                $(this).parent('.paragraph').fadeOut(500, function() {
+                    $(this).find('.active').removeClass('active').fadeOut(0, function () {
+                        $('.h3-dinamic .dinamic-text').eq(0).addClass('active').fadeIn(0, function(){
+                            $(this).parent('.paragraph').fadeIn(500);
+                        });
+                    });
+                });
+                return false;
+            }
         }
     });
 }
 
 function initCallsCounter() {
-    var 
+    var
         $countBlock = $('.block.user-count .count'),
         countValue = $countBlock.data('value');
 
@@ -96,12 +125,12 @@ function initCallsCounter() {
     }, 3000);
 
     function setNewValue (value) {
-        var 
+        var
             stringValue = value.toString(),
             digitNumber = 1;
 
         for (var i = stringValue.length - 1; i >= 0; i--) {
-            var 
+            var
                 $digitItem = $('<div class="digit-item">'+ stringValue[i] +'</div>'),
                 $digit = $countBlock.find('.digit:nth-last-of-type(' + digitNumber + ')'),
                 $oldDigitItem = $digit.find('.digit-item');
@@ -140,7 +169,7 @@ function initCallsCounter() {
                     }, 700);
                 })($digitItem, $oldDigitItem);
             }
-            
+
             digitNumber++;
         }
     }
